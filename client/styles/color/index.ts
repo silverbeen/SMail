42312847fbf9c652dbec203ff8906635e89356c3
강은빈 <kub0803@naver.com>
2022-03-10 22:13:33 +0900

    [FEAT] color assets 추가

---
+++ client/styles/color/index.ts
@@ -0,0 +1,7 @@
+export const mainColor = "#557DF2";
+export const blueColor = "#6F828C";
+export const grayTextColor = "#AAAAAA";
+export const grayBorderColor = "#ECECEC";
+export const redColor = "#EC7575";
+export const mintColor = "#EEF6FB";
+export const mintBlueColor = "#9DB7C5";
