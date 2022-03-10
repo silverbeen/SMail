95cdec61b303d60aa61822eadc2d6d791290e071
강은빈 <kub0803@naver.com>
2022-03-10 09:57:48 +0900

    [FEAT] 엔티티 설정

---
+++ server/src/entities/Mail.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './User';
+
+@Index('mail_id_UNIQUE', ['mailId'], { unique: true })
+@Index('fk_mail_user1_idx', ['userId'], {})
+@Entity('mail', { schema: 'smail_db' })
+export class Mail {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'mail_id', unsigned: true })
+  mailId: number;
+
+  @Column('varchar', { name: 'user_id', length: 30 })
+  userId: string;
+
+  @Column('varchar', { name: 'mail_content', length: 255 })
+  mailContent: string;
+
+  @ManyToOne(() => User, (user) => user.mail, {
+    onDelete: 'NO ACTION',
+    onUpdate: 'NO ACTION',
+  })
+  @JoinColumn([{ name: 'user_id', referencedColumnName: 'userId' }])
+  user: User;
+}
