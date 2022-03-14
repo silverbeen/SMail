c94b9e2f6ffda62e4b7c97087aa8a825827fc363
강은빈 <kub0803@naver.com>
2022-03-14 16:56:05 +0900

    [FIX] cors 수정

--- server/src/apis/category/category.service.ts
+++ server/src/apis/category/category.service.ts
@@ -6,7 +6,6 @@ import { CategoryList } from '../../entities/CategoryList';
 import { Content } from '../../entities/Content';
 
 // 실질적인 로직 짜는 곳
-
 @Injectable()
 export class CategoryService {
   constructor(
@@ -27,14 +26,6 @@ export class CategoryService {
           title: category.title,
           list: [],
         };
-        // .createQueryBuilder('category')
-        // .innerJoin('category.contents', 'content')
-        // .where('category.category_id = :id ', {
-        //   id: category.categoryListId,
-        // })
-        // .addSelect('COUNT(*) as total')
-        // .groupBy('category.field_id')
-        // .getRawMany();
         const categories = await this.categoryRepository
           .createQueryBuilder('content')
           .select('content.field_id', 'fieldId')
@@ -44,22 +35,16 @@ export class CategoryService {
           .where('content.category_id = :id', { id: category.categoryListId })
           .groupBy('content.field_id')
           .getRawMany();
-        //   where: {
-        //     categoryId: category.categoryListId,
-        //   },
-        // });
 
-        // categories.map(async (content) => {
-        //   const contentCnt = await this.contentRepository
-        //     .createQueryBuilder('content')
-        //     .where('content.field_id = :id', { id: content.fieldId })
-        //     .getCount();
+        // const categories= category.map(async (content) => {
+        //     const contentCnt = await this.contentRepository
+        //       .createQueryBuilder('content')
+        //       .where('content.field_id = :id', { id: content.fieldId })
+        //       .getCount();
 
-        // content.('contentCnt', contentCnt);
-        // console.log(contentCnt);
-        //test.list = categories;
+        test.list = categories;
 
-        return categories;
+        return test;
       }),
     );
 
