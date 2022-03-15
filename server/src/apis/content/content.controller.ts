9fd75add22034bce6d5aada9ce6da9d953062f8e
강은빈 <kub0803@naver.com>
2022-03-15 20:08:55 +0900

    [FEAT] 카테고리 필터링 기능 구현

--- server/src/apis/content/content.controller.ts
+++ server/src/apis/content/content.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ContentService } from './content.service';
 
 @Controller('content')
-export class ContentController {}
+export class ContentController {
+  constructor(private readonly contentService: ContentService) {}
+
+  // 카테고리 필터링
+  @Get()
+  async getContentList(@Query('id') id: number) {
+    return await this.contentService.getContentList(id);
+  }
+}
--- server/src/apis/content/content.service.ts
+++ server/src/apis/content/content.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Content } from '../../entities/Content';
 
 @Injectable()
-export class ContentService {}
+export class ContentService {
+  constructor(
+    @InjectRepository(Content)
+    private contentRepository: Repository<Content>,
+  ) {}
+
+  async getContentList(id: number) {
+    return await this.contentRepository.find({ fieldId: id });
+  }
+}
