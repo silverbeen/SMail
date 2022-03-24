2ca2e724b35fb45a304b4ab045c703112bcc2e5b
강은빈 <kub0803@naver.com>
2022-03-24 23:04:32 +0900

    [FIX] toast ui 수정

--- client/lib/function/toast.ts
+++ client/lib/function/toast.ts
@@ -1,25 +1,28 @@
 import { toast } from "react-toastify";
+import { mainColor } from "../../styles/color";
 
 export function ToastSuccess(text: string | null) {
   toast.success(`${text}`, {
-    position: "top-right",
-    autoClose: 1000,
-    hideProgressBar: false,
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: true,
     closeOnClick: true,
-    pauseOnHover: true,
+    pauseOnHover: false,
     draggable: true,
     progress: undefined,
+    style: { fontSize: "14px" },
   });
 }
 
 export function ToastError(text: string | null) {
   toast.error(`${text}`, {
-    position: "top-right",
-    autoClose: 1000,
-    hideProgressBar: false,
+    position: "top-center",
+    autoClose: 1500,
+    hideProgressBar: true,
     closeOnClick: true,
-    pauseOnHover: true,
+    pauseOnHover: false,
     draggable: true,
     progress: undefined,
+    style: { fontSize: "14px" },
   });
 }
