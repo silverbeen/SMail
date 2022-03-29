dcaee9a58de2a1f10a374404c03ee89a65e8b174
강은빈 <kub0803@naver.com>
2022-03-29 23:54:17 +0900

    [FEAT] iconBox eventHandle 수정

--- client/components/mailWrite/mailInput/IconBox.tsx
+++ client/components/mailWrite/mailInput/IconBox.tsx
@@ -3,18 +3,25 @@ import { mintBlueColor } from "../../../styles/color";
 
 type Props = {
   icon: "Copy" | "Save" | "Plus";
-  templateModalOpen?: () => void;
+  templateModalOpen?: any;
+  mailSaveHandle?: any;
 };
 
-const IconBox = ({ icon, templateModalOpen }: Props) => {
+const IconBox = ({ icon, templateModalOpen, mailSaveHandle }: Props) => {
   const IconNameChangeHandle = (icon: "Copy" | "Save" | "Plus") => {
     if (icon == "Copy") return "Copy";
     else if (icon == "Save") return "Save";
     else return "템플릿 추가하기";
   };
 
+  const iconClickHandle = () => {
+    if (icon == "Copy") return;
+    else if (icon == "Save") mailSaveHandle();
+    else return templateModalOpen();
+  };
+
   return (
-    <IconBoxContainer onClick={templateModalOpen}>
+    <IconBoxContainer onClick={iconClickHandle}>
       <img src={`/assets/icon/${icon}Icon.svg`} alt={`${icon} 아이콘`} />
       <span>{IconNameChangeHandle(icon)}</span>
     </IconBoxContainer>
