994c58bdea249181e7141983fc206b20f140b6c9
강은빈 <kub0803@naver.com>
2022-03-30 23:53:07 +0900

    [FEAT] content interface

--- client/lib/types/ContentTypes.ts
+++ client/lib/types/ContentTypes.ts
@@ -2,4 +2,11 @@ export interface ContentType {
   contentId: number;
   fieldId: number;
   content: string;
+  id: number;
+}
+
+export interface DeskContentType {
+  id: number;
+  content: string;
+  content_id: string;
 }
