877ad64818cfe3799765164247831f6aec401d00
강은빈 <kub0803@naver.com>
2022-03-31 23:53:31 +0900

    [FATE] template , desk, content API 연동

--- client/components/content/contentBox/index.tsx
+++ client/components/content/contentBox/index.tsx
@@ -1,20 +1,15 @@
 import styled from "@emotion/styled";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { useQuery } from "react-query";
 import { useRecoilState } from "recoil";
 import { MAIN_URL } from "../../../lib/api/common";
-import { ToastSuccess } from "../../../lib/function/toast";
-import { MailInputAtom } from "../../../lib/module/atom/mail";
+import { contentMenuAtom } from "../../../lib/module/atom/content";
+import { blueColor, grayBorderColor, mainColor } from "../../../styles/color";
 import { ContentType } from "../../../lib/types/ContentTypes";
-import {
-  blueColor,
-  grayBorderColor,
-  mainColor,
-  mintBlueColor,
-  mintColor,
-} from "../../../styles/color";
+import ContentItemBox from "./ContentItemBox";
+import desk from "../../../lib/api/desk";
+import template from "../../../lib/api/template";
 
 const menuData = [
   { id: 1, menu: "문구" },
@@ -24,10 +19,13 @@ const menuData = [
 
 const ContentBox = () => {
   const router = useRouter();
-  const [mailValue, setMailValue] = useRecoilState(MailInputAtom);
-  const [selected, setSelected] = useState<number>(1);
+  const [optionMenu, setSelected] = useRecoilState(contentMenuAtom);
 
-  const { data: contentData, isLoading } = useQuery(
+  const menuClickHandle = (id: number) => {
+    setSelected(id);
+  };
+
+  const { data: contentData } = useQuery(
     ["contentData", router.query.id],
     () => axios(`${MAIN_URL}/content?id=${router.query.id}`),
     {
@@ -37,22 +35,27 @@ const ContentBox = () => {
     }
   );
 
-  const addIconClickHanle = (contentId: number) => {
-    const textValue = contentData?.data.find(
-      (content: ContentType) => contentId === content.contentId
-    )?.content;
+  const { data: deskData } = useQuery(["deskData"], () => desk.getDesk(), {
+    cacheTime: Infinity,
+    staleTime: Infinity,
+  });
 
-    setMailValue({
-      ...mailValue,
-      content: mailValue.content + "\n" + textValue,
-    });
+  const { data: templateData } = useQuery(
+    ["templateData"],
+    () => template.getTemplate(),
+    {
+      cacheTime: Infinity,
+      staleTime: Infinity,
+    }
+  );
 
-    ToastSuccess("✏️문구가 추가되었습니다.");
-  };
-
-  const menuClickHandle = (id: number) => {
-    setSelected(id);
-  };
+  function menuReturn() {
+    if (optionMenu === 1) {
+      return contentData?.data;
+    } else if (optionMenu === 2) {
+      return deskData?.data.deskContent;
+    } else return templateData?.data;
+  }
 
   return (
     <ContentBoxContainer>
@@ -63,8 +66,8 @@ const ContentBox = () => {
               key={menu.id}
               onClick={() => menuClickHandle(menu.id)}
               style={{
-                color: menu.id === selected ? `${mainColor}` : "",
-                fontWeight: menu.id === selected ? 500 : "",
+                color: menu.id === optionMenu ? `${mainColor}` : "",
+                fontWeight: menu.id === optionMenu ? 500 : "",
               }}
             >
               {menu.menu}
@@ -72,24 +75,28 @@ const ContentBox = () => {
           ))}
         </MenuContainer>
         <ContentItemList>
-          {contentData?.data.map((content: ContentType) => (
-            <ContentItemBox key={content.contentId}>
-              <pre>{content.content}</pre>
-              <div className="item_box">
-                <img
-                  src="/assets/icon/CopyIcon.svg"
-                  alt=""
-                  onClick={() => addIconClickHanle(content.contentId)}
-                />
-                <span>Saved</span>
-              </div>
-            </ContentItemBox>
+          {menuReturn()?.map((item: ContentType) => (
+            <ContentItemBox
+              key={item.id}
+              contentData={contentData?.data}
+              content={item}
+            />
           ))}
         </ContentItemList>
       </ContentWrapper>
     </ContentBoxContainer>
   );
 };
+
+const ContentItemList = styled.div`
+  height: 85%;
+  max-height: 66vh;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  align-items: center;
+`;
 
 const ContentBoxContainer = styled.article`
   width: 500px;
@@ -120,54 +127,6 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 23px;
-`;
-
-const ContentItemBox = styled.div`
-  width: 90%;
-  background: ${mintColor};
-  box-shadow: 0px 1px 12px rgba(200, 200, 200, 0.25);
-  border-radius: 10px;
-  padding: 14px 18px;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  gap: 20px;
-
-  & pre {
-    color: ${blueColor};
-    font-size: 14px;
-    font-weight: 400;
-    white-space: pre-wrap;
-  }
-
-  .item_box {
-    gap: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-
-    > span {
-      cursor: pointer;
-      font-size: 12px;
-      font-weight: 700;
-      color: ${mintBlueColor};
-    }
-
-    > img {
-      height: 16px;
-      cursor: pointer;
-    }
-  }
-`;
-
-const ContentItemList = styled.div`
-  height: 85%;
-  max-height: 66vh;
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-  align-items: center;
 `;
 
 export default ContentBox;
