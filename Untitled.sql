d963fc62090980b8d351b98c34ce4103353d4c63
강은빈 <kub0803@naver.com>
2022-04-03 23:51:56 +0900

    [FIX] fix database

---
+++ Untitled.sql
@@ -0,0 +1,6 @@
+select * from smail_db.user_desk;
+select * from smail_db.desk_content;
+select * from smail_db.template;
+select * from smail_db.mail;
+
+insert into smail_db.template values(2, 'qwer', '제목', '내용');
