702c27db40aaa93a34cdc001df2d4ed96ab31e09
강은빈 <kub0803@naver.com>
2022-05-19 11:12:39 +0900

    [FIX] gudie

--- client/pages/guide.tsx
+++ client/pages/guide.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import type { NextPage } from "next";
+import GuideCategory from "../components/guide/catrogry/GuideCategory";
 import Header from "../components/header";
 
 const MailGuide: NextPage = () => {
@@ -10,13 +11,11 @@ const MailGuide: NextPage = () => {
         메일 가이드를 통하여
         <br /> 쉽고 빠르게 메일을 보내요!
       </h1>
-      <GuideCategory>
-        <span>메일 예절</span>
-        <span>답장률 높이는 제안</span>
-        <span>공고 메일</span>
-        <span>비지니스 메일</span>
-        <span>섭외 메일</span>
-      </GuideCategory>
+      <GuideCategory />
+      <SelectedGuide>{}</SelectedGuide>
+      <GuideContainer>
+        <h1>강은빈님께서 보냈던 첫번째 제안 메일</h1>
+      </GuideContainer>
     </HomeConrainer>
   );
 };
@@ -32,30 +31,25 @@ const HomeConrainer = styled.div`
   justify-content: center;
   gap: 100px;
 
-  h1 {
+  > h1 {
     text-align: center;
     color: white;
     font-weight: 500;
   }
 `;
 
-const GuideCategory = styled.section`
-  width: 40%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  color: white;
-  row-gap: 20px;
-  column-gap: 70px;
-
-  span {
-    font-size: 18px;
-    color: #eef6fb;
-    cursor: pointer;
+const GuideContainer = styled.section`
+  width: 50%;
+  padding: 20px;
+  box-sizing: border-box;
+  border: 1px solid red;
+  background: white;
+  color: black;
+  h1 {
+    font-weight: 500;
   }
+`;
 
-  span:nth-child(2) {
-    font-size: 24px;
-    color: white;
-  }
+const SelectedGuide = styled.h1`
+
 `;
