f1cb5b32dfcae3f6d161ad6d50b82593bfb7f136
강은빈 <kub0803@naver.com>
2022-05-28 23:38:25 +0900

    [DELETE] Saved 엔티티 삭제

--- server/src/entities/Saved.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity('saved', { schema: 'smail_db' })
-export class Saved {
-  @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
-  id: number;
-}
