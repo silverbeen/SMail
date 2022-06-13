a397f79c671993f9eb6297a5c8110a6b24f4cb12
강은빈 <kub0803@naver.com>
2022-06-13 23:23:00 +0900

    [FIX] scroll에 따라 헤더 스타일 변경

--- client/pages/guide.tsx
+++ client/pages/guide.tsx
@@ -1,33 +1,46 @@
 import styled from "@emotion/styled";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import GuideCategory from "../components/guide/catrogry/GuideCategory";
+import GuideBox from "../components/guide/content/GuideBox";
 import Header from "../components/header";
 
 const MailGuide: NextPage = () => {
+  const [offset, setOffset] = useState<number>(0);
+  const [changeColor, setChangeColor] = useState<boolean>(false);
+  const [selectCategory, setSelectCategory] = useState<string>("메일 예절");
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset);
+    };
+  }, []);
+
+  useEffect(() => {
+    offset > 130 ? setChangeColor(true) : setChangeColor(false);
+  }, [offset]);
+
   return (
     <HomeConrainer>
-      <Header />
+      <Header changeColor={changeColor} />
       <h1>
         메일 가이드를 통하여
         <br /> 쉽고 빠르게 메일을 보내요!
       </h1>
-      <GuideCategory />
-      <SelectedGuide>{}</SelectedGuide>
-      <GuideContainer>
-        <h1>강은빈님께서 보냈던 첫번째 제안 메일</h1>
-      </GuideContainer>
+      <GuideCategory setSelectCategory={setSelectCategory} />
+      <GuideBox selectCategory={selectCategory} />
     </HomeConrainer>
   );
 };
 export default MailGuide;
 
 const HomeConrainer = styled.div`
+  padding: 150px 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-
   justify-content: center;
   gap: 100px;
 
@@ -36,20 +49,4 @@ const HomeConrainer = styled.div`
     color: white;
     font-weight: 500;
   }
-`;
-
-const GuideContainer = styled.section`
-  width: 50%;
-  padding: 20px;
-  box-sizing: border-box;
-  border: 1px solid red;
-  background: white;
-  color: black;
-  h1 {
-    font-weight: 500;
-  }
-`;
-
-const SelectedGuide = styled.h1`
-
 `;
